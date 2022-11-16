d24019827c875f4e81c8f040bf3c08b24e8cfbfd
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-16 16:37:07 +0100

    first setup commit

---
+++ pages/api/_data/products.js
@@ -0,0 +1,20 @@
+export const products = [
+    {
+     id: 1,
+     name: "Pasta",
+     category: "Food",
+     detail: "Leckere Pasta",
+    },
+    {
+     id: 2,
+     name: "Wine",
+     category: "Drinks",
+     detail: "Einfach unwiderstehlich",
+    },
+    {
+     id: 3,
+     name: "Chair",
+     category: "Furniture",
+     detail: "Top Produkt",
+    },
+];
